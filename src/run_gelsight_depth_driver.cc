6bd0d81844de455a975937ceca0cacc1ebc4f8d0
Gregory Izatt <gizatt@mit.edu>
2017-08-21 17:20:13 -0400

    Support saving raw images to requested directory

--- src/run_gelsight_depth_driver.cc
+++ src/run_gelsight_depth_driver.cc
@@ -1,4 +1,6 @@
 #include <unistd.h>
+#include <cstdlib>
+#include <iomanip>
 #include <iostream>
 #include <random>
 #include <stdexcept>
@@ -20,6 +22,10 @@ int main(int argc, char** argv) {
        {"video",
         {"-v", "--video"},
         "Video device #? (default: -1 (automatic))",
+        1},
+       {"save_raws",
+        {"-s", "--saveraws"},
+        "Directory in which to save raw images (default: none)",
         1}}};
 
   argagg::parser_results args;
@@ -40,19 +46,45 @@ int main(int argc, char** argv) {
     return -1;
   }
 
+  string save_raws_dir = "";
+  if (args["save_raws"]) {
+    save_raws_dir = args["save_raws"].as<string>();
+    ostringstream command;
+    command << "mkdir -p ";
+    command << save_raws_dir;
+    int error = system(command.str().c_str());
+    if (error == -1) {
+      printf("Error creating directory %s\n", save_raws_dir.c_str());
+      return -1;
+    }
+  }
+
   string video_source = "-1";
   if (args["video"]) video_source = args["video"].as<string>();
 
   GelsightDepthDriver driver(video_source);
 
-  cv::namedWindow("RawImage", cv::WINDOW_AUTOSIZE);
+  cv::namedWindow("Raw Image", cv::WINDOW_AUTOSIZE);
   cv::startWindowThread();
 
+  int num_images = 0;
   while (1) {
     driver.GetNewWebcamImage();
     auto last_image = driver.get_last_raw_image();
     if (last_image.rows > 0 && last_image.cols > 0) {
-      cv::imshow("RawImage", driver.get_last_raw_image());
+      cv::imshow("Raw Image", driver.get_last_raw_image());
+
+      // Save raw image, if requested.
+      if (save_raws_dir.size() > 0) {
+        ostringstream output_filename;
+        output_filename << save_raws_dir;
+        output_filename << "/";
+        output_filename << setfill('0') << setw(7) << num_images;
+        output_filename << ".jpg";
+        imwrite(output_filename.str(), last_image);
+      }
+
+      num_images++;
     }
     usleep(1000);
   }
